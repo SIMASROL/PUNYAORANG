aadda8bae5a44ac05484c21433c7dfac5dc0085e
masrulhadi <simasrolhadi@gmail.com>
2024-10-23 19:38:43 +0700

    punyaorang

---
+++ satu.java
@@ -0,0 +1,6 @@
+public class satu{
+  public static void
+  main(String[] args) {
+    System.out.println("pala bapakkau");
+  }
+}
---
+++ ujicoba.java
@@ -0,0 +1,10 @@
+public class ujicoba {
+  public static void main(String[] args) {
+    String nama = "NAZWA AMANAZIA";
+    String nim = "23146067";
+    String ttl = "5 januari 2025, neraka jahannam";
+    System.out.println("Nama :" +nama);
+    System.out.println("NIM :" +nim);
+    System.out.println("TTL :" +ttl);
+  }
+}
